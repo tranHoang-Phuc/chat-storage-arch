b8b7af45490b93aecac69d216ef58072eede1eaf
tranHoang-Phuc <thphuc110523@gmail.com>
2025-09-13 15:19:10 +0700

    Initial commit

---
+++ src/main/java/com/sds/phucth/chatstoragearch/ChatStorageArchApplication.java
@@ -0,0 +1,13 @@
+package com.sds.phucth.chatstoragearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatStorageArchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatStorageArchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sds/phucth/chatstoragearch/ChatStorageArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.phucth.chatstoragearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatStorageArchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
